64bd8d33025caf836a6ddaaf1d8dc995f304e2f5
Heiko Geppert <heiko.geppert@ipvs.uni-stuttgart.de>
2024-05-24 16:08:32 +0200

    tighten release time bounds for transmissions and queueing

--- constraint_programming/cp_structs.py
+++ constraint_programming/cp_structs.py
@@ -51,15 +51,16 @@ class CpVariables:
             self.pcp[stream.id] = expression.integer_var(0, network.min_queues_available - 2, name=f"pcp_{stream.id}")
 
             # create transmission_window and queuing variables
+            running_arrival_time = 0
             for egress_port in routes[stream.id]:
                 for frame in iterate_frames_per_hc(stream, scenario.hyper_cycle):
-                    release_time = frame * stream.cycle_time_ns
+                    release_time = frame * stream.cycle_time_ns + running_arrival_time
                     deadline = release_time + stream.max_delay_ns
 
                     self.transmission_windows[egress_port.id].append(
                         expression.interval_var(
-                            start=(release_time, deadline),  # todo make the bound tighter
-                            end=(release_time, deadline),  # todo make the bound tighter
+                            start=(release_time, deadline),  # todo make the deadline bound tighter
+                            end=(release_time, deadline),  # todo make the deadline bound tighter
                             size=egress_port.calculate_transmission_delay_in_ns_of(stream),
                             optional=False,
                             name='transmission_port_{}_stream_{}_frame_{}'.format(egress_port.id, stream.id, frame)
@@ -67,12 +68,15 @@ class CpVariables:
                     for queue in range(0, network.min_queues_available - 1):
                         self.queuing[egress_port.id][queue].append(
                             expression.interval_var(
-                                start=(release_time, deadline),  # todo make the bound tighter
-                                end=(release_time, deadline),  # todo make the bound tighter
+                                start=(release_time, deadline),  # todo make the deadline bound tighter
+                                end=(release_time, deadline),  # todo make the deadline tighter
                                 optional=True,
                                 name='queuing_port_{}_stream_{}_frame_{}_queue_{}'.format(egress_port.id, stream.id,
                                                                                           frame, queue)
                             ))
+
+                running_arrival_time += egress_port.calculate_transmission_delay_in_ns_of(stream)
+                running_arrival_time += egress_port.propagation_delay_ns
 
     def get_transmission_var(self, hop: EgressPort, stream: Stream, frame: int):
         return next((v for v in self.transmission_windows[hop.id] if
