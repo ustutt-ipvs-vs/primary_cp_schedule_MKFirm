5e0727234bb49173ddb4de40b565c17addb36114
Heiko Geppert <heiko.geppert@ipvs.uni-stuttgart.de>
2025-06-18 11:47:58 +0200

    Update help text and readme

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from network.network_graph import NetworkGraph
 from scenario.scenario import Scenario
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--network", type=str, help="Path to the network graph file", required=True)
-parser.add_argument("-s", "--scenario", type=str, help="Path to the flow scenario file", required=True)
+parser.add_argument("-n", "--network", type=str, help="Path to the network graph file")
+parser.add_argument("-s", "--scenario", type=str, help="Path to the flow scenario file")
 parser.add_argument("-t", "--timelimit", type=int,
                     help="solver time limit in seconds. Use negative values for unlimited.", default=120)
 parser.add_argument("--threads", type=int, help="Number of threads to be used at most", default=4)
-parser.add_argument("-v", "--verbose", help="print a lot of debug outputs. Is overwritten by the raw flag.",
+parser.add_argument("-v", "--verbose", help="print a lot of debug outputs on the console. Can be overwritten by the raw flag.",
                     action='store_true')
-parser.add_argument("--raw-output", help="If set, the output will be in a raw format. Overwrites the verbose flag.",
+parser.add_argument("--raw-output", help="If set, the console output will be in a raw format. Overwrites the verbose flag.",
                     action='store_true')
-parser.add_argument("-o", "--output", type=str, help="Path to the output file", default='transmission_output.json')
-parser.add_argument("--cplex", type=str, help="Path to cplex executable", default=None)
+parser.add_argument("-o", "--output", type=str, help="Filename and path to store the output in.", default='transmission_output.json')
+parser.add_argument("--cplex", type=str, help="Path to cplex executable. Provide if it is not in the path variable.", default=None)
 
 args = parser.parse_args()
 raw_output = args.raw_output
@@ -39,6 +39,6 @@ if '/' in str(args.output):
     if output_dir and not os.path.isdir(output_dir):
         print(f"Creating directory {output_dir} for output file.")
         os.makedirs(output_dir, exist_ok=True)
-    
+
 if result.is_solution():
     write_result_to_json(result, parameters, args.output)
